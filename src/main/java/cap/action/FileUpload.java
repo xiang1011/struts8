1897c7453cd171d730510bce76a9b61d69424fd9
xiang1011 <2659840026@qq.com>
2018-09-26 19:58:49 +0800

    文件上传

---
+++ src/main/java/cap/action/FileUpload.java
@@ -0,0 +1,35 @@
+package cap.action;
+
+import java.io.*;
+
+import static com.opensymphony.xwork2.Action.SUCCESS;
+
+public class FileUpload {
+    private  static final long serialVeresionUID=1L;
+    private File file;
+    private String fileFileName;
+    public  File getFile(){
+        return  file;
+    }
+    public  void setFile(File file){
+        this.file=file;
+    }
+    public  String getFileFileName(){
+        return fileFileName;
+    }
+    public void  setFileFileName(String fileFileName){
+        this.fileFileName=fileFileName;
+    }
+    public  String execute()throws Exception{
+        InputStream is=new FileInputStream(file);
+        OutputStream os=new FileOutputStream("d://"+fileFileName);
+        byte[]buffer=new byte[1024];
+        int length=0;
+        while ((length=is.read(buffer))!=-1){
+            os.write(buffer,0,length);
+        }
+        os.close();
+        is.close();
+        return SUCCESS;
+    }
+}
